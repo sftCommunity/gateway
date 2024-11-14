2e116ea21f2e99552a8833b8126058317c404d92
David Agámez <davidagamez04@gmail.com>
2024-11-14 18:50:46 -0500

    chore: add dotenv and joi dependencies, and configure environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+
+  logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
