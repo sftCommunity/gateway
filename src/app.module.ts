fe99825f3ccec6a98a34d260a2b8a24fc772d01a
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 15:49:24 -0500

    feat: Created AuthController with register, login, and verify endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { NatsModule } from './transports/nats.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NatsModule],
+  imports: [ProductsModule, OrdersModule, NatsModule, AuthModule],
   controllers: [],
   providers: [],
 })
