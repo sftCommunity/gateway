902695f0308d1769fa7a576136f399c4d8f1e50e
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-15 21:46:02 -0500

    refactor: update environment variables to include NATS_SERVERS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,19 +3,13 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  PRODUCTS_MICROSERVICE_HOST: string;
-  PRODUCTS_MICROSERVICE_PORT: number;
-  ORDERS_MICROSERVICE_HOST: string;
-  ORDERS_MICROSERVICE_PORT: number;
+  NATS_SERVERS: string;
 }
 
 const envsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
-    ORDERS_MICROSERVICE_HOST: joi.string().required(),
-    ORDERS_MICROSERVICE_PORT: joi.number().required(),
+    NATS_SERVERS: joi.string().required(),
   })
   .unknown(true);
 
@@ -27,8 +21,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
-  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICE_HOST,
-  ordersMicroservicePort: envVars.ORDERS_MICROSERVICE_PORT,
+  natsServers: envVars.NATS_SERVERS,
 };
