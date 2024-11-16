ba848a1f23a73c4905402313acaf306ab18a0c4e
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 18:51:41 -0500

    refactor: Update AuthController to handle register and login requests with request validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,31 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  register() {
-    return this.client.send('auth.register.user', {});
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((e) => {
+        throw new RpcException(e);
+      }),
+    );
   }
 
   @Post('login')
-  login() {
-    return this.client.send('auth.login.user', {});
+  login(@Body() loginUserDto: LoginUserDto) {
+    // return this.client.send('auth.login.user', loginUserDto);
+    return loginUserDto;
   }
 
   @Get('verify')
   verifyToken() {
-    return this.client.send('auth.verify.user', {});
+    // return this.client.send('auth.verify.user', {});
+    return {};
   }
 }
