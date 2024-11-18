724660b43b6483ef399d49b86451d68f6ce4f89f
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-18 16:13:45 -0500

    fix: update Auth decorator to use AuthGuard class instead of instantiation

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,12 +1,12 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { RoleProtected } from './role-protected.decorator';
 import { UserRoleGuard } from '../guards/user-role.guard';
 import { ValidRoles } from '../interfaces/valid-roles';
+import { AuthGuard } from '../guards';
 
 export function Auth(...roles: ValidRoles[]) {
   return applyDecorators(
     RoleProtected(...roles),
-    UseGuards(AuthGuard(), UserRoleGuard),
+    UseGuards(AuthGuard, UserRoleGuard),
   );
 }
