018fdf2e86ca79574ca662fe3b83bbf9fecf7710
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-27 04:26:25 -0500

    refactor: Update UserController to use UserPaginationDto for findAll method

--- src/auth/user/user.controller.ts
+++ src/auth/user/user.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
-import { PaginationDto } from 'src/common/dto';
 import { NATS_SERVICE } from 'src/config';
+import { UserPaginationDto } from './dto/user-pagination.dto';
 
 @Controller('user')
 export class UserController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Get('find_all')
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.client.send('user.find.all', paginationDto).pipe(
+  findAll(@Query() userPaginationDto: UserPaginationDto) {
+    return this.client.send('user.find.all', userPaginationDto).pipe(
+      catchError((e) => {
+        throw new RpcException(e);
+      }),
+    );
+  }
+
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.client.send('user.find.one', term).pipe(
       catchError((e) => {
         throw new RpcException(e);
       }),
