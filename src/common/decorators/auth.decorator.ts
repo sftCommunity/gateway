63a9f69629c86ff31ef29874402d7467167fa107
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-27 00:00:33 -0500

    refactor: Reorder imports in auth decorator

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
-import { UserRoleGuard } from '../guards/user-role.guard';
-import { ValidRoles } from '../../auth/interfaces/valid-roles';
 import { AuthGuard } from '../../auth/guards';
+import { ValidRoles } from '../../auth/interfaces/valid-roles';
+import { UserRoleGuard } from '../guards/user-role.guard';
 import { RoleProtected } from './role-protected.decorator';
 
 export function Auth(...roles: ValidRoles[]) {
