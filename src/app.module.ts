e62efa53af088d758932eae06db47a2b54153b87
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:56:55 -0500

    refactor: Reorganize module imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { FilesModule } from './files/files.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { NatsModule } from './transports/nats.module';
-import { AuthModule } from './auth/auth.module';
-import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [ProductsModule, OrdersModule, NatsModule, AuthModule, FilesModule],
