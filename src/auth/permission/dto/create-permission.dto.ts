fca7ce5b20718fd530ed25ef1981dda61b96bae8
David <davidagamez04@gmail.com>
2025-03-20 17:24:47 -0500

    feat: add Swagger API documentation for CreatePermissionDto

--- src/auth/permission/dto/create-permission.dto.ts
+++ src/auth/permission/dto/create-permission.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePermissionDto {
+  @ApiProperty({
+    description: 'The name of the permission',
+    example: 'read_users',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
