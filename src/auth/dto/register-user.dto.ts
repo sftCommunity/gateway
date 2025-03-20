1bbda8a3f48a12c6aa5184b56bfd5b00b001a92a
David <davidagamez04@gmail.com>
2025-03-20 17:24:14 -0500

    feat: enhance RegisterUserDto with Swagger API documentation

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -5,19 +5,39 @@ import {
   IsString,
   IsStrongPassword,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'johndoe@example.com',
+  })
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'StrongP@ssw0rd!',
+    minLength: 8,
+    format: 'password',
+  })
   @IsString()
   @IsStrongPassword()
   password: string;
 
+  @ApiProperty({
+    description: 'Is the user active?',
+    default: true,
+    example: true,
+  })
   @IsBoolean()
   @IsOptional()
   is_active: boolean = true;
