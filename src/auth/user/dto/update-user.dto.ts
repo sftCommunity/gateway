d8bb64034b0f9a13a82e46e39cde051ed868c92d
David <davidagamez04@gmail.com>
2025-03-20 17:26:44 -0500

    feat: add Swagger API documentation for UpdateUserDto

--- src/auth/user/dto/update-user.dto.ts
+++ src/auth/user/dto/update-user.dto.ts
@@ -2,6 +2,17 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsString, IsUUID } from 'class-validator';
 import { RegisterUserDto } from 'src/auth/dto';
 
+/**
+ * Data Transfer Object (DTO) for updating a user.
+ * Extends the `RegisterUserDto` with optional fields using `PartialType`.
+ *
+ * @extends PartialType(RegisterUserDto)
+ *
+ * @swagger
+ * @property {string} id - The unique identifier of the user.
+ *   - Must be a valid UUID.
+ *   - Must be a string.
+ */
 export class UpdateUserDto extends PartialType(RegisterUserDto) {
   @IsString()
   @IsUUID()
