248f32d89b3b2cd77aa5b8d4625ae66f5a551aee
David <davidagamez04@gmail.com>
2025-03-20 17:24:19 -0500

    feat: add Swagger API documentation for JwtPayload interface

--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -1,3 +1,22 @@
+/**
+ * Interface representing the payload of a JSON Web Token (JWT).
+ * 
+ * @swagger
+ * components:
+ *   schemas:
+ *     JwtPayload:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the user.
+ *         name:
+ *           type: string
+ *           description: Full name of the user.
+ *         email:
+ *           type: string
+ *           description: Email address of the user.
+ */
 export interface JwtPayload {
   id: string;
   name: string;
