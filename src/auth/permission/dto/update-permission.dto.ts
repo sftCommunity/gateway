34a8c2beb666d5bb4e4ab5ca039d95eca3b25bd9
David <davidagamez04@gmail.com>
2025-03-20 17:26:30 -0500

    feat: add Swagger API documentation for UpdatePermissionDto

--- src/auth/permission/dto/update-permission.dto.ts
+++ src/auth/permission/dto/update-permission.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePermissionDto } from './create-permission.dto';
 
+/**
+ * Data Transfer Object (DTO) for updating a permission.
+ * 
+ * This class extends the `PartialType` of `CreatePermissionDto`, 
+ * making all properties of `CreatePermissionDto` optional for updates.
+ * 
+ * @extends PartialType(CreatePermissionDto)
+ * 
+ * @swagger
+ * @description DTO used for updating permission details. All fields are optional.
+ */
 export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {}
