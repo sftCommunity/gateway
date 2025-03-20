7871d1a2ddeba2964322cb4902155d68f2371839
David <davidagamez04@gmail.com>
2025-03-20 17:24:01 -0500

    feat: refine Bearer authentication setup in Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Gateway API')
     .setVersion('1.0')
-    .addBearerAuth() // Add token input
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
